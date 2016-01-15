129d21fbf20260a3d4ba40e9449f9effe0d91805
srey <srey@vspdev-srey.artinsoft.com>
2016-01-15 17:33:48 -0600

    added new constants for Messages of Win32 API purpose

--- FXLab/src/fxlab/win32/User32.java
+++ FXLab/src/fxlab/win32/User32.java
@@ -19,6 +19,26 @@ public interface User32 extends com.sun.jna.platform.win32.User32{
      * Singleton instance of this Interface
      */
     User32 INSTANCE_API= (User32) Native.loadLibrary("user32", User32.class, W32APIOptions.DEFAULT_OPTIONS);
+    
+    /**
+     * Constant for determines the length, in characters, of the text associated with a window.
+     */
+    final int WM_GETTEXTLENGTH= 0x000E;
+    
+    /**
+     * Constant for copy the text that corresponds to a window into a buffer provided by the caller.
+     */
+    final int WM_GETTEXT= 0x000D;
+    
+    /**
+     * Constant for gets the length of a string in a list box.
+     */
+    final int LB_GETTEXTLEN = 0x018A;
+    
+    /**
+     * Constant for gets a string from a list box.
+     */
+    final int LB_GETTEXT = 0x0189;
         
     /**
      * Retrieves a handle to a window whose class name and window name match the specified strings. 
@@ -99,6 +119,35 @@ public interface User32 extends com.sun.jna.platform.win32.User32{
      *      The return value specifies the result of the message processing; it depends on the message sent.
      */    
     int SendMessage(HWND hWnd, int Msg, int wParam, HANDLE lParam);
+    
+    /**
+     * Sends the specified message to a window or windows. The SendMessage function calls the window 
+     * procedure for the specified window and does not return until the window procedure has processed the message.
+     * To send a message and return immediately, use the SendMessageCallback or SendNotifyMessage function. 
+     * To post a message to a thread's message queue and return immediately, use the PostMessage or PostThreadMessage function.
+     * 
+     * @param hWnd
+     *          A handle to the window whose window procedure will receive the message. If this parameter is HWND_BROADCAST ((HWND)0xffff), 
+     *          the message is sent to all top-level windows in the system, including disabled or invisible unowned windows, overlapped windows, 
+     *          and pop-up windows; but the message is not sent to child windows.
+     * 
+     *          Message sending is subject to UIPI. The thread of a process can send messages only to message queues of threads in 
+     *          processes of lesser or equal integrity level.
+     * 
+     * @param Msg
+     *         The message to be sent.
+     *         For lists of the system-provided messages, see System-Defined Messages.
+     * 
+     * @param wParam
+     *          Additional message-specific information.
+     * 
+     * @param lParam
+     *          Additional message-specific information.
+     * 
+     * @return 
+     *      The return value specifies the result of the message processing; it depends on the message sent.
+     */    
+    int SendMessage(HWND hWnd, int Msg, int wParam, char[] lParam);
     
     /**
      * Enumerates all entries in the property list of a window by passing them, one by one, to the specified callback function. 
