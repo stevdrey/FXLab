f9a7804fd9f55eb96fd903de0531b9c60196fa2e
srey <srey@vspdev-srey.artinsoft.com>
2016-01-18 11:28:31 -0600

    created a enum class for identified the controls or windows in constants

---
+++ FXLab/src/fxlab/ui/enu/TypeControl.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fxlab.ui.enu;
+
+/**
+ *
+ * @author srey
+ */
+public enum TypeControl {
+    TEXT_BOX, LIST_BOX, COMBO_BOX, LABLE, BUTTON;
+}
